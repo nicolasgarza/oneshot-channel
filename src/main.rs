1a71a38c334f2da7c13a52288cbbaacd5345e339
Nicolas Garza <nicolas.garza@gmail.com>
2024-08-08 14:16:23 -0500

    blocking

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::{
     cell::UnsafeCell,
+    marker::PhantomData,
     mem::MaybeUninit,
     sync::atomic::{
         AtomicBool,
         Ordering::{Acquire, Relaxed, Release},
     },
-    thread,
+    thread::{self, Thread},
 };
 
 /*
@@ -20,10 +21,12 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
 
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
+    receiving_thread: Thread,
 }
 
 pub struct Receiver<'a, T> {
     channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>,
 }
 
 pub struct Channel<T> {
@@ -41,7 +44,16 @@ impl<T> Channel<T> {
 
     pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
         *self = Self::new();
-        (Sender { channel: self }, Receiver { channel: self })
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
     }
 }
 
@@ -60,6 +72,7 @@ impl<T> Sender<'_, T> {
     pub fn send(self, message: T) {
         unsafe { (*self.channel.message.get()).write(message) };
         self.channel.ready.store(true, Release);
+        self.receiving_thread.unpark();
     }
 }
 
@@ -67,9 +80,10 @@ impl<T> Receiver<'_, T> {
     pub fn is_ready(&self) -> bool {
         self.channel.ready.load(Relaxed)
     }
+
     pub fn receive(self) -> T {
         if !self.channel.ready.swap(false, Acquire) {
-            panic!("no message available!");
+            thread::park();
         }
         unsafe { (*self.channel.message.get()).assume_init_read() }
     }
@@ -79,14 +93,9 @@ fn main() {
     let mut channel = Channel::new();
     thread::scope(|s| {
         let (sender, receiver) = channel.split();
-        let t = thread::current();
         s.spawn(move || {
             sender.send("hello world!");
-            t.unpark();
         });
-        while !receiver.is_ready() {
-            thread::park();
-        }
         assert_eq!(receiver.receive(), "hello world!");
     })
 }
